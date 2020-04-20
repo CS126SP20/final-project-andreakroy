6772cbd68b84ac4f2ca299417eb95f40477ee465
andreakroy <akroy2@illinois.edu>
2020-04-19 17:51:43 -0700

    implemented Pawn class

--- include/chess/piece.h
+++ include/chess/piece.h
@@ -9,25 +9,52 @@
 #include <tuple>
 #include <vector>
 
+namespace piece {
+
 using std::tuple;
 using std::vector;
 
-namespace piece {
 enum class PieceType { kKing, kQueen, kRook, kPawn, kKnight, kBishop };
 
 enum class Color { kBlack, kWhite };
 
+// Abstract Class representing a piece. Each peace has a type and color and
+// implements CanMove and Path.
 class Piece {
  public:
+  // Piece constructor taking in a PieceType and Color enum.
   Piece(const PieceType t, const Color c);
+  // The piece type.
   const PieceType type_;
-  virtual bool CanMove(const size_t x_old, const size_t y_old,
-                       const size_t x_new, const size_t y_new) const = 0;
-  virtual vector<tuple<size_t, size_t>> Path(const size_t x_old,
-                                             const size_t y_old,
-                                             const size_t x_new,
-                                             const size_t y_new) const = 0;
+  // Returns whether or not a piece could make a move from (x_old, y_old) to
+  // (x_new, y_new). This method only checks the geometric  plausibility of
+  // such a move and does not account for factors such as pieces being in the
+  // way or the king being in check.
+  virtual auto CanMove(const size_t x_old, const size_t y_old,
+                       const size_t x_new, const size_t y_new) const
+      -> bool = 0;
+  // Returns a vector of (x,y) tuples representing positions on a chessboard.
+  // Returns the hypothetical path a piece would take in travelling from
+  // (x_old, y_old) to (x_new, y_new). Precondition: the move must be a valid
+  // move.
+  virtual auto Path(const size_t x_old, const size_t y_old, const size_t x_new,
+                    const size_t y_new) const
+      -> vector<tuple<size_t, size_t>> = 0;
+  // The piece color (white or black).
   const Color color_;
+};
+
+// Class representing a pawn object.
+class Pawn : public Piece {
+ public:
+  // Pawn override of the CanMove piece method.
+  auto CanMove(const size_t x_old, const size_t y_old, const size_t x_new,
+               const size_t y_new) const -> bool override;
+  // Pawn override of the Path piece method.
+  auto Path(const size_t x_old, const size_t y_old, const size_t x_new,
+            const size_t y_new) const -> vector<tuple<size_t, size_t>> override;
+  // Pawn constructor, creating a Pawn Piece with the given color.
+  explicit Pawn(const Color c);
 };
 
 }  // namespace piece
--- src/piece.cc
+++ src/piece.cc
@@ -1,7 +1,65 @@
-// Copyright (c) 2020 [Your Name]. All rights reserved.
+// Copyright (c) 2020 Andrea Roy. All rights reserved.
 
+#include <cassert>
 #include <chess/piece.h>
+
+using std::make_tuple;
 
 namespace piece {
 Piece::Piece(const PieceType t, const Color c) : type_(t), color_(c) {}
-}  // namespace mylibrary
+
+Pawn::Pawn(const Color c) : Piece(PieceType::kPawn, c) {}
+auto Pawn::CanMove(const size_t x_old, const size_t y_old, const size_t x_new,
+                   const size_t y_new) const -> bool {
+  int x_diff = x_new - x_old;
+  int y_diff = y_new - y_old;
+  if (y_diff >= 0 && color_ == Color::kBlack) {
+    // Checks that a black pawn is not moving backwards.
+    return false;
+  } else if (y_diff <= 0 && color_ == Color::kWhite) {
+    // Checks that a white pawn is not moving backwards.
+    return false;
+  }
+  // Gets the max size of a pawn move (either one or two squares) depending on
+  // the pawn's original position.
+  size_t max_y = 1;
+  if ((y_old == 1 && color_ == Color::kWhite ||
+       y_old == 6 && color_ == Color::kBlack) &&
+      x_diff == 0) {
+    max_y = 2;
+  }
+  if (abs(y_diff) > max_y) {
+    // Checks that the pawn move is not exceeding the maximum allowable
+    // distance.
+    return false;
+  } else if (abs(x_diff) > 1) {
+    // Checks that the pawn move is not moving more than one square
+    // horizontally.
+    return false;
+  }
+  return true;
+}
+vector<tuple<size_t, size_t>> Pawn::Path(const size_t x_old, const size_t y_old,
+                                         const size_t x_new,
+                                         const size_t y_new) const {
+  assert(CanMove(x_old, y_old, x_new, y_new));
+  vector<tuple<size_t, size_t>> path;
+  // Gets the maximum size of the move.
+  size_t max_y = 1;
+  if ((y_old == 1 && color_ == Color::kWhite ||
+       y_old == 6 && color_ == Color::kBlack) &&
+      x_old - x_new == 0) {
+    max_y = 2;
+  }
+  // Stores the direction of movement.
+  int factor = 1;
+  if (color_ == Color::kBlack) {
+    factor = -1;
+  }
+  for (int i = y_old + factor; i <= y_new; i += factor) {
+    path.emplace_back(make_tuple(x_new, i));
+  }
+  return path;
+}
+
+}  // namespace piece
